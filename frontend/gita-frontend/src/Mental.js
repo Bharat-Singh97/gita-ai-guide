c31fcebba7a81d4a60acabace0b38512c816b351
Bharat Singh Panwar <119155310+Bharat-Singh97@users.noreply.github.com>
2026-03-27 02:42:22 +0530

    connect frontend to live backend

--- frontend/gita-frontend/src/Mental.js
+++ frontend/gita-frontend/src/Mental.js
@@ -11,7 +11,7 @@ function Mental() {
 
   const getAdvice = async (text = input) => {
   try {
-    const res = await fetch("http://127.0.0.1:5000/chat", {
+    const res = await fetch("https://gita-backend-prqd.onrender.com/chat", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ query: text })
